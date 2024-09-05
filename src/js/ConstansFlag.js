a8926c25364cf0f0aec3f7c060aaee4146f48e51
yangmingsen <yangmingsen16@outlook.com>
2024-09-05 13:29:59 +0800

    修改prov配置

--- src/js/ConstansFlag.js
+++ src/js/ConstansFlag.js
@@ -1,6 +1,6 @@
 export const ConstansFlag = {
     itemList : {rencentFiles: 0, delFiles: 1, treeFiles: 2},
-    apiUrl: 'http://api.notetest.yms.top/note',
+    apiUrl: 'http://api.note.yms.top/note',
     fileType: {jpg: 'jpg', wer: 'wer', docx: 'docx', markdown: 'md', png: 'png',
         unknow: 'unknow', jpeg: 'jpeg', pdf: 'pdf', xlsx: 'xlsx', mindmap: 'mindmap'}
 
