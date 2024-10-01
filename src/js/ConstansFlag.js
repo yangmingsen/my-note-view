53541d5044498ac033c5c67a78b792363a955169
yangmingsen <yangmingsen16@outlook.com>
2024-10-01 09:07:42 +0800

    修改prov参数

--- src/js/ConstansFlag.js
+++ src/js/ConstansFlag.js
@@ -1,7 +1,7 @@
 export const ConstansFlag = {
     // 最近文件：0， 最近删除：1，我的目录树：2
     itemList : {rencentFiles: 0, delFiles: 1, treeFiles: 2},
-    apiUrl: 'http://api.notetest.yms.top/note',
+    apiUrl: 'http://api.note.yms.top/note',
     fileType: {jpg: 'jpg', wer: 'wer', docx: 'docx', markdown: 'md', png: 'png',
         unknow: 'unknow', jpeg: 'jpeg', pdf: 'pdf', xlsx: 'xlsx', mindmap: 'mindmap'},
     username: 'username',
