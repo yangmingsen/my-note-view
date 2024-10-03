2d97b6786d488bbf5cc02e53e2007cb1d6a1e85a
yangmingsen <yangmingsen16@outlook.com>
2024-10-03 20:04:07 +0800

    修复一些异常 1.如果是加密笔记，那么右击菜单时，不应该出现复制预览地址 2. 在tree的子目录上新建目录，新建完成后，tree目录加载不正常

--- src/store/useItemSelectStore.js
+++ src/store/useItemSelectStore.js
@@ -1,7 +1,8 @@
 import { defineStore } from 'pinia'
 import {ref} from 'vue'
 
-
+//当点击left1面板某个item时，通知文件列表组件进行动态更新
+//通知文件列表组件更新
 export const useItemSelectStore = defineStore('itemSelectId', () => {
     //用于
     const itemSelectKey = ref(-1)
--- src/store/useNotifyUpdateFileListStore.js
+++ src/store/useNotifyUpdateFileListStore.js
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import {ref} from 'vue'
 
+//用于通知更新FileList组件更新
 export const useNotifyUpdateFileListStore = defineStore('notifyUpdateFileListId', () => {
     //用于保存通知事件
     const updateFileList = ref(0)
